d9e42c74bb66c22b27e42aa4e116a9eb1c909ca4
Daniel Vogel <dan@nonsequitoria.com>
2021-07-02 14:31:16 -0400

    moved CNAM

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     // eleventyConfig.setTemplateFormats([ "html" ]);
 
     eleventyConfig.addPassthroughCopy("src/img");
+    eleventyConfig.addPassthroughCopy("src/CNAME");
 
     // eleventyConfig.addPassthroughCopy({
     //     './node_modules/alpinejs/dist/alpine.js': './assets/alpine.js',
